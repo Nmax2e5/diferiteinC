434a6bca5e0d3445be47fbc82e0a624c70a871ff
Nmax2e5 <74609995+Nmax2e5@users.noreply.github.com>
2020-12-13 18:41:41 +0200

    windows

--- windows.c
+++ windows.c
@@ -1,15 +1,10 @@
 void textcolor (int color)
 {
     static int __BACKGROUND;
-
     HANDLE h = GetStdHandle ( STD_OUTPUT_HANDLE );
     CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
-
-
     GetConsoleScreenBufferInfo(h, &csbiInfo);
-
-    SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE),
-                             color + (__BACKGROUND << 4));
+    SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE),color + (__BACKGROUND << 4));
 }
 
 void clc()
@@ -18,4 +13,14 @@ COORD cursorPosition;
 cursorPosition.X = 0;
 cursorPosition.Y = 0;
 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+}
+
+void delay(double number_of_seconds)
+{
+    double milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds)
+    {
+        //cazul cu exceptii
+    }
 }
