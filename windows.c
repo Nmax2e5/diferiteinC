005fb478e4046673b139c136aa34eaa9191b6cd2
Nmax2e5 <74609995+Nmax2e5@users.noreply.github.com>
2020-12-14 19:54:55 +0200

    finalcurent

--- windows.c
+++ windows.c
@@ -3,9 +3,14 @@
 #include <windows.h>
 #include <time.h>
 
-void setcolor (WORD color)
+HANDLE hin;
+HANDLE hout;
+INPUT_RECORD InputRecord;
+DWORD Events;
+
+void setcolor (int color)
 {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),14);
 
     //     0 = Black
     //     1 = Blue
@@ -48,6 +53,38 @@ void initWin(int width, int height)
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
 }
 
+void clc(){
+    COORD coordScreen = { 0, 0 };
+    DWORD cCharsWritten;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    DWORD dwConSize;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+    FillConsoleOutputCharacter(hConsole, TEXT(' '), dwConSize, coordScreen, &cCharsWritten);
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    FillConsoleOutputAttribute(hConsole, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten);
+    SetConsoleCursorPosition(hConsole, coordScreen);
+    return;
+}
+
+int mouseClick()
+{
+    hin = GetStdHandle(STD_INPUT_HANDLE);
+    ReadConsoleInput(hin, &InputRecord, 1, &Events);
+
+    if(InputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+        return 1;
+    return 0;
+}
+
+COORD coord()
+{
+    COORD aux = {InputRecord.Event.MouseEvent.dwMousePosition.X, InputRecord.Event.MouseEvent.dwMousePosition.Y};
+    return aux;
+}
+
 //functiile per project
 
 
@@ -69,16 +106,14 @@ int main()
     initWin(800,600);
     while(1)
     {
-        setPos(0,0);
+        resizeConsole(800, 600);
         setcolor(4);
-        HANDLE hin = GetStdHandle(STD_INPUT_HANDLE);
-        INPUT_RECORD InputRecord;
-        DWORD Events;
-        ReadConsoleInput(hin, &InputRecord, 1, &Events);
 
-        if(InputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+        if(mouseClick())
         {
-            printf("%d %d", InputRecord.Event.MouseEvent.dwMousePosition.X, InputRecord.Event.MouseEvent.dwMousePosition.Y);
+            COORD cur = coord();
+            clc();
+            printf("%d %d", cur.X, cur.Y);
         }
 
     }
